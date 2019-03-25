fe9ff23d84d611cee02d1a2b761a7674d960d565
Ismael Villegas <ismael@entraencatarsis.com>
2019-03-25 12:53:46 -0700

    Pero que buenos terminos

---
+++ app/terminos.php
@@ -0,0 +1,20 @@
+<?php
+  $page_title = "Inicio";
+  include('estructura/cabecera.php');
+
+  
+?>
+
+<main class="terminos-page">
+  <div class="container">
+    <h1> Términos y condiciones </h1>
+    <p> I. Tu nos pagas. Nosotros te entregamos, capichi? </p>
+    <p> II. Tu te enfermas. Nosotros no existimos, capichi? </p>
+    <p> III. Nosotros te cobramos. Tu no te awitas, capichi? </p>
+    <p> Gracias, bye. </p>
+  </div>
+</main>
+
+<?php
+  include('estructura/pie.php');
+?>
